b8dfcf0e2b935d64266316b0d16104e98d8e72ca
xX_EpicHackerDude_Xx <113366399+RichardM1337@users.noreply.github.com>
2023-10-13 13:16:28 -0400

    i b workin

--- dom.js
+++ dom.js
@@ -2,17 +2,17 @@
 DOMSelectors = {
   // store related code for elements here
   form: document.querySelector("#form"),
-  firstName: document.querySelector(".first-name"),
+  firstName: document.querySelector("firstName"),
   h2s: document.querySelector("h2"),
   // in quotes, use html notation
 };
 console.log(DOMSelectors.firstName);
 
 // waits for form to submit, could be click, page load, input
-DOMSelectors.form.addEventListener("submit", function (event) {
-  event.preventDefault(); //stps page refresh
-  DOMSelectors.h2s.forEach(
-    // fix this
-    (header) => (header.textContent = DOMSelectors.firstName.value)
-  );
-});
+DOMSelectors.form.addEventListener("submit"),
+  function (event) {
+    event.preventDefault(); //stps page refresh
+    DOMSelectors.h2s.forEach(
+      (header) => (header.textContent = DOMSelectors.firstName.value)
+    );
+  };
